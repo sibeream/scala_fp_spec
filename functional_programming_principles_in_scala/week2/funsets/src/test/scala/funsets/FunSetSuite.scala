acbeaeb30fa00badea2e88c10754ae00c442ba97
Artem Egorenkov <aegorenkov.91@gmail.com>
2021-11-09 19:40:12 +0100

    week2 solution

--- functional_programming_principles_in_scala/week2/funsets/src/test/scala/funsets/FunSetSuite.scala
+++ functional_programming_principles_in_scala/week2/funsets/src/test/scala/funsets/FunSetSuite.scala
@@ -36,6 +36,7 @@ class FunSetSuite extends munit.FunSuite:
     val s1 = singletonSet(1)
     val s2 = singletonSet(2)
     val s3 = singletonSet(3)
+    val s4 = singletonSet(4)
 
   /**
    * This test is currently disabled (by using @Ignore) because the method
@@ -44,7 +45,7 @@ class FunSetSuite extends munit.FunSuite:
    * Once you finish your implementation of "singletonSet", remove the
    * .ignore annotation.
    */
-  test("singleton set one contains one".ignore) {
+  test("singleton set one contains one") {
 
     /**
      * We create a new instance of the "TestSets" trait, this gives us access
@@ -66,6 +67,73 @@ class FunSetSuite extends munit.FunSuite:
       assert(!contains(s, 3), "Union 3")
   }
 
+  test("intersection contains only elements all elements that are both sets") {
+    new TestSets:
+      val u1 = union(union(s1, s2), s3)
+      val u2 = union(union(s2, s3), s4)
+      val s = intersect(u1, u2)
+      assert(!contains(s, 1), "Intersection 1")
+      assert(contains(s, 2), "Intersection 2")
+      assert(contains(s, 3), "Intersection 3")
+      assert(!contains(s, 4), "Intersection 4")
+  }
+
+  test("diff contains all the elements of the first set that are not in the second set") {
+    new TestSets:
+      val u1 = union(union(s1, s2), s3)
+      val u2 = union(union(s2, s3), s4)
+      val s = diff(u1, u2)
+      assert(contains(s, 1), "Diff 1")
+      assert(!contains(s, 2), "Diff 2")
+      assert(!contains(s, 3), "Diff 3")
+      assert(!contains(s, 4), "Diff 4")
+  }
+
+  test("filter returns the subset of elements for which predicate holds") {
+    new TestSets:
+      val u1 = union(union(s1, s2), s3)
+      val u2 = union(union(s2, s3), s4)
+      val u = union(u1, u2)
+      val s = filter(u, x => x % 2 == 0)
+      assert(!contains(s, 1), "Filter 1")
+      assert(contains(s, 2), "Filter 2")
+      assert(!contains(s, 3), "Filter 3")
+      assert(contains(s, 4), "Filter 4")
+  }
+
+  test("filter returns the subset of elements for which predicate holds") {
+    new TestSets:
+      val u1 = union(union(s1, s2), s3)
+      val u2 = union(union(s2, s3), s4)
+      val s = union(u1, u2)
+      assert(forall(s, x => x > 0), "Filter 1")
+      assert(!forall(s, x => x % 2 == 0), "Filter 2")
+  }
+
+  test("filter returns the subset of elements for which predicate holds") {
+    new TestSets:
+      val u1 = union(union(s1, s2), s3)
+      val u2 = union(union(s2, s3), s4)
+      val s = union(u1, u2)
+      assert(exists(s, x => x == 3), "Exists 1")
+      assert(!exists(s, x => x == 5), "Exists 2")
+  }
+
+  test("filter returns the subset of elements for which predicate holds") {
+    new TestSets:
+      val u1 = union(union(s1, s2), s3)
+      val u2 = union(union(s2, s3), s4)
+      val u = union(u1, u2)
+      val s = map(u, x => -x)
+      assert(!contains(s, 1), "Map 1")
+      assert(!contains(s, 2), "Map 2")
+      assert(!contains(s, 3), "Map 3")
+      assert(!contains(s, 4), "Map 4")
+      assert(contains(s, -1), "Map 5")
+      assert(contains(s, -2), "Map 6")
+      assert(contains(s, -3), "Map 7")
+      assert(contains(s, -4), "Map 8")
+  }
 
 
   import scala.concurrent.duration.*
